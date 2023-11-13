651f12375ab13de16d3e7598ca5588f787cf2772
Arbdoul <salisuabdullahi64@gmail.com>
2023-11-13 21:21:43 +0100

    test driver in your area  components

--- src/components/Home/ShopByCarType.tsx
+++ src/components/Home/ShopByCarType.tsx
@@ -1,7 +1,6 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable, Image } from "react-native";
 import React from "react";
 import { createStyleSheet, useStyles } from "../../theme";
-import TopBrands from "./TopBrands";
 import Header from "../reuseComponents/Header";
 import Cars from "../reuseComponents/Cars";
 
@@ -11,6 +10,21 @@ const ShopByCarType = () => {
     <>
       <Header title="Shop By Car Type" subtitle="view all" />
       <Cars />
+      <View style={styles.container}>
+        <Text style={styles.title}>Test drive in your area</Text>
+        <Text style={styles.body}>
+          Drive from your home or {"\n"} a carline hub
+        </Text>
+
+        <View>
+          <Pressable style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>view Cars</Text>
+          </Pressable>
+        </View>
+        <View style={styles.image}>
+          <Image source={require("../../../assets/images/car.png")} />
+        </View>
+      </View>
     </>
   );
 };
@@ -19,19 +33,46 @@ export default ShopByCarType;
 
 const stylesheet = createStyleSheet((theme) => ({
   container: {
-    marginTop: 24,
+    position: "relative",
+    borderRadius: 16,
+    borderWidth: 1,
+    height: 158,
+    width: 350,
+    marginLeft: 24,
+    margin: 24,
+    backgroundColor: theme.colors.gray900,
   },
-  topBrandContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginLeft: 16,
-    marginRight: 16,
+  title: {
+    ...theme.typography.bodyLarge.bold,
+    color: theme.colors.white,
+    marginTop: 20,
+    marginLeft: 20,
   },
-  topBrandTitle: {
-    ...theme.typography.bodyXLarge.bold,
+  body: {
+    ...theme.typography.bodySmall.medium,
+    color: theme.colors.white,
+    margin: 20,
   },
-  topBrandSubtitle: {
-    ...theme.typography.bodyMedium.medium,
-    color: theme.colors.gray500,
+  buttonContainer: {
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: theme.colors.white,
+    width: 106,
+    height: 32,
+    padding: 16,
+    left: 24,
+    marginTop: -10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    position: "absolute",
+    ...theme.typography.bodySmall.bold,
+    color: theme.colors.white,
+  },
+  image: {
+    position: "absolute",
+    top: 25,
+    left: 193,
   },
 }));
